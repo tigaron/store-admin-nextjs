e1f93e1f47b2c4a8a293668760ed664be22ed26a
tigaron <suputra@mail.com>
2023-10-06 11:12:03 +0700

    feat: add create store modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
 import { UserButton } from "@clerk/nextjs";
 
+import { useStoreModal } from "@/hooks/use-store-modal";
+
 export default function RootPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div>
+      Root Page
       <UserButton afterSignOutUrl="/" />
     </div>
-  )
+  );
 }
